29961a406960f70a4a510316dc114d78b4a5b9e4
Artem Kurdikov <kourtema@gmail.com>
2025-05-04 16:41:37 +0300

    bugfix: relative .env path for server

--- server/go.mod
+++ server/go.mod
@@ -4,6 +4,7 @@ go 1.24.2
 
 require (
 	github.com/gofiber/fiber/v2 v2.52.6
+	github.com/joho/godotenv v1.5.1
 	go.uber.org/zap v1.27.0
 )
 
--- server/main.go
+++ server/main.go
@@ -2,18 +2,28 @@ package main
 
 import (
 	"os"
+	"path/filepath"
 
 	"github.com/KTemka1234/Yadro-Impulse-2025/server/config"
 	"github.com/KTemka1234/Yadro-Impulse-2025/server/handlers"
 	"github.com/KTemka1234/Yadro-Impulse-2025/server/logger"
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/cors"
+	"github.com/joho/godotenv"
 	"go.uber.org/zap"
 )
 
 func main() {
 	log := logger.New(true)
 	defer log.Sync()
+
+	envPath := filepath.Join("..", ".env")
+
+	// Загружаем переменные
+	err := godotenv.Load(envPath)
+	if err != nil {
+		log.Error("Failed to load .env file:", zap.Error(err))
+	}
 
 	os.MkdirAll(config.StoragePath, 0755) // see chmod 0755 for more info
 
